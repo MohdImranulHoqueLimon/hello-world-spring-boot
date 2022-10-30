6596550653ffe1154626b0288fceadd3615bbaf7
Mohd Imranul Hoque Limon <limon1036@gmail.com>
2022-10-30 21:54:36 +0100

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
   @GetMapping("/home")
   public String home() {
-    String str = "This is my home Germany, Frankfurt";
+    String str = "This is my home Germany, Frankfurt 11";
     System.out.println(str);
     return str;
   }
